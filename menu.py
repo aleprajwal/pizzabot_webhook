d302df7796fdaa580406bfdc5f80ca0e4da1d005
Prajwal Ale <aleprajwal11@gmail.com>
2019-02-19 11:54:18 +0545

    new changes

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
-PIZZA_MENU = {'size':{'Large':20,'medium':15,'small':10},
-			  'topping':{'cheese':5, 'pepperoni':10, 'mushroom':15, 'susage':20}}
+PIZZA_MENU = {'size':{'extra large':25,'large':20,'medium':15,'small':10},
+			  'topping':{'cheese':5, 'pepperoni':10, 'mushrooms':15, 'sausage':20, 'olive':20, 'bacon':15}}
 
 BREADSTICK_MENU = {'New York Brand Original':5,'Pillsbury':7,'Granforno Grissini':8,'Angonoa':10,'Olive Garden':12}
 
@@ -8,4 +8,3 @@ DRINK_MENU = {'Cappuccino':15, 'Latte':20,'coffee':5,'Tea':5,'Espresso':10,'Hot 
 
 DESSERT_MENU = {'Salted Caramel Sunda':20,'Turkish Delight Sundae':25,'Mini Cho Sundae':30,'Hot Cho Fudge Brownies':25,
 				'Over Baked Churros':20,'Chocolate Dipping Sause':15}
-
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 webhook for pizza ordering chatbot
+
+-->> Procfile for deploying on Heroku
+-->> app.yaml for deploying on google app engine
